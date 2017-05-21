c54dbfafcb8aa098cab04e376e5c02400978232d
SimonBo <szymon.borucki@gmail.com>
2017-05-21 02:50:50 +0200

    add jsonapi_utils

--- app/controllers/api/v1/rentals_controller.rb
+++ app/controllers/api/v1/rentals_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::RentalsController < JSONAPI::ResourceController
+  include JSONAPI::Utils
+
+  def index
+    do_some_sophisticated_thing
+    jsonapi_render json: Rental.all
+  end
+
+  private
+
+  def do_some_sophisticated_thing
+    p 'Hello world!'
+  end
 end
